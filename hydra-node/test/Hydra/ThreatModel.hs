a7db095d9e11e00977bf0d94cbc8869f2844f84e
Ulf Norell <ulf.norell@gmail.com>
2024-02-15 16:25:58 +0100

    Hook up to quickcheck-threatmodel

---
+++ hydra-node/test/Hydra/ThreatModel.hs
@@ -0,0 +1,30 @@
+
+module Hydra.ThreatModel where
+
+import Prelude
+import Data.Coerce
+
+import Cardano.Api.UTxO as Hydra
+import Hydra.Chain.Direct.Fixture qualified as Fixture
+import Hydra.Chain.Direct.Contract.Abort
+
+import Test.QuickCheck
+import Test.QuickCheck.ThreatModel
+import Test.QuickCheck.ThreatModel.TxModifier ()
+
+makeThreatModelEnv :: (Tx Hydra.Era, Hydra.UTxO) -> ThreatModelEnv
+makeThreatModelEnv (tx, utxo) = ThreatModelEnv
+  { currentTx    = tx
+  , currentUTxOs = coerce utxo
+  , pparams      = LedgerProtocolParameters Fixture.pparams
+  }
+
+someThreatModel :: ThreatModel ()
+someThreatModel = do
+  inp <- pickAny =<< getTxReferenceInputs
+  shouldValidate $ removeInput inp    -- Fails!
+  pure ()
+
+prop_something :: Property
+prop_something = runThreatModel someThreatModel [makeThreatModelEnv healthyAbortTx]
+
